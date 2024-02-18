2569f3191ee735fc7d61a3a54a35325428482668
dvirerental <dvirer11111@gmail.com>
2024-02-18 14:27:12 +0200

    1

--- Headers/PreAssembler.h
+++ Headers/PreAssembler.h
@@ -3,10 +3,7 @@
 #include "general.h"
 #include "nodes.h"
 #define MAX_LINE_LENGTH 81
-#define ENDMCR_LETTERS 7
 #define MAX_WORD_LENGTH 10
-void copyFile(FILE*);
 FILE* preAssemble(FILE*);
 int createMacro(FILE*, char*, int, int);
-int skip(int, FILE*);
 int isFileIndication(const char*);
