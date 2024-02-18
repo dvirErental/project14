186cbad1bab93d2113e31207e5ac5a3299a42950
ShaiHazony <shaihazony@gmail.com>
2024-02-18 14:28:27 +0200

    0

--- Sourcefiles/firstPass.c
+++ Sourcefiles/firstPass.c
@@ -1,10 +1,10 @@
 #include "../Headers/firstPass.h"
-
+/*assuming max number of words in a line is 6*/
 line_table firstPass(FILE* fp) {
     int IC = 0, DC = 0;
     int address = 100;
     boolean isFirst = true;
-    char *line[MAX_LINE_LENGTH];
+    char line[MAX_LINE_LENGTH];
     char* firstWord = mallocError(sizeof(int)*MAX_WORD_LENGTH);
     char* secondWord = mallocError(sizeof(int)*MAX_WORD_LENGTH);
     char* thirdWord = mallocError(sizeof(int)*MAX_WORD_LENGTH);
@@ -13,9 +13,13 @@ line_table firstPass(FILE* fp) {
     char* sixthWord = mallocError(sizeof(int)*MAX_WORD_LENGTH);
     while (!feof(fp)) {
         fgets(line, MAX_LINE_LENGTH, fp);
-        if (sscanf(line, "%s%s%s%s%s%s", firstWord, secondWord, thirdWord, fourtheWord, fifthWord, sixthWord)) {
-            if (strcmp(firstWord, ".define"){
-                if (isFirst == true ) /*continue from here*/
+        if (sscanf(line, "%s%s%s%s%s%s", firstWord, secondWord, thirdWord, fourthWord, fifthWord, sixthWord)) {
+            if (strcmp(firstWord, ".define")){
+                if (isFirst == true){
+                    make_line_table(secondWord, "mdefine", atoi(thirdWord));
+                }
+                else
+                    add_to_list(secondWord, "mdefine", atoi(thirdWord));
             }
 
         }
