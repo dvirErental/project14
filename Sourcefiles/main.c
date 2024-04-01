7076f99c9d0609d822eb392e540de06f52164f67
ShaiHazony <shaihazony@gmail.com>
2024-04-01 15:50:25 +0300

    0

--- Sourcefiles/main.c
+++ Sourcefiles/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "../Headers/PreAssembler.h"
-#include "../Headers//firstPass.h"
-//
+#include "../Headers/firstPass.h"
+
 int main(){
     FILE* pa = fopen("../TextFiles/input1", "r");
-    preAssemble(pa);
+    FILE* ppa = preAssemble(pa);
+    fclose(ppa);
     printf("the code worked");
     FILE* fp = fopen("../TextFiles/postPreAssembler", "r");
     firstPass(fp);
